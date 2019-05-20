3dbb3362c42795efbdbe06107de033960b878852
egartech <egartech>
2019-05-20 12:38:54 +0300

    Change in selects

--- sql/5_select.sql
+++ sql/5_select.sql
@@ -8,10 +8,6 @@ SELECT c.name AS 'Имя', o.date_of_completion 'Дата завершения',
 	FROM PartsStore.order o, PartsStore.client c 
 	WHERE c.id = o.client_id AND date_of_completion BETWEEN '2019-05-20 00:00:00' AND '2019-05-31 23:59:59';
 
-SELECT c.name AS 'Клиент', o.order_date AS 'Дата заказа', e.surname AS 'Сотрудник', m.description AS 'Описание модели'
-	FROM  PartsStore.order o, PartsStore.client c, PartsStore.employee e, PartsStore.model m
-	WHERE c.id = o.client_id AND e.surmame=(SELECT surname FROM PartsStore.employee e WHERE e.position = 'агент 007' OR e.position = 'агент 008' ); 
-	
 SELECT c.name AS 'Клиент', o.order_date AS 'Дата заказа', e.surname AS 'Сотрудник', m.description AS 'Описание модели', e.position
 	FROM  PartsStore.order o, PartsStore.client c, PartsStore.employee e, PartsStore.model m, PartsStore.product p
 	WHERE c.id = o.client_id AND e.id = o.employee_id AND m.model=p.model AND p.serial_number=o.serial_number
